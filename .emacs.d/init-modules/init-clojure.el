81ce6bcffdaa360ffcde69087a2abf1bfa79b6ff
Matthew Wampler-Doty <matthew.wampler.doty@gmail.com>
2015-02-14 13:35:21 -0800

    formatting

--- .emacs.d/init-modules/init-clojure.el
+++ .emacs.d/init-modules/init-clojure.el
@@ -6,11 +6,11 @@
 
 ;; Clojure mode
 (require 'bootstrap)
-(require-package 'clojure-mode 
-		 'flycheck
-		 'evil
-		 ;'kibit-mode
-		 )
+(require-package 'clojure-mode
+         'flycheck
+         'evil
+         ;'kibit-mode
+         )
 
 ;;;; EVIL mode
 (require-package 'evil 'cider)
@@ -20,11 +20,11 @@
 (evil-define-key 'normal clojure-mode-map (kbd "M-,") 'cider-jump-back)
 (evil-define-key 'motion clojure-mode-map "gd" 'cider-jump)
 (evil-define-key 'motion clojure-mode-map "gb" 'cider-jump-back)
-(add-hook 'clojure-mode-hook
-	  (lambda ()
-	    ;; Type :ns to change namespaces in cider repl
-	    (evil-ex-define-cmd "ns" 'cider-repl-set-ns)
-	   ))
+(add-hook
+ 'clojure-mode-hook
+ (lambda ()
+   ;; Type :ns to change namespaces in cider repl
+   (evil-ex-define-cmd "ns" 'cider-repl-set-ns)))
 
 
 ;;;; Use light-table's command-return for evaluating in the REPL
@@ -34,8 +34,8 @@
     (cond
      (mark-active (cider-eval-region (region-beginning) (region-end)))
      ((equal evil-state 'normal) (progn (forward-char)
-					(cider-eval-last-sexp)
-					(backward-char)))
+                    (cider-eval-last-sexp)
+                    (backward-char)))
      (t (cider-eval-last-sexp)))))
 
 (define-key clojure-mode-map
@@ -44,8 +44,8 @@
     (cond
      (mark-active (cider-insert-in-repl (buffer-substring-no-properties (region-beginning) (region-end)) t))
      ((equal evil-state 'normal) (progn (forward-char)
-					(cider-insert-last-sexp-in-repl t)
-					(backward-char)))
+                    (cider-insert-last-sexp-in-repl t)
+                    (backward-char)))
      (t (cider-insert-last-sexp-in-repl t)))))
 
 
