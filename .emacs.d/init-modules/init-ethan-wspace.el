8ee03b14c69c42933a330558e3fc2001fb1dcc0e
Matthew Wampler-Doty <matthew.wampler.doty@gmail.com>
2015-02-14 13:34:28 -0800

    untabbing on save

--- .emacs.d/init-modules/init-ethan-wspace.el
+++ .emacs.d/init-modules/init-ethan-wspace.el
@@ -7,11 +7,16 @@
 (require 'bootstrap)
 (require-package 'ethan-wspace)
 (setq mode-require-final-newline nil)
-(setq-default tab-width 4 indent-tabs-mode t)
+(setq-default tab-width 4 indent-tabs-mode nil)
 (add-hook 'prog-mode-hook 'ethan-wspace-mode)
 (add-hook
  'makefile-mode-hook
  '(lambda () (setq ethan-wspace-errors
               (remove 'tabs ethan-wspace-errors))))
+(add-hook
+ 'before-save-hook
+ (lambda () (if (not indent-tabs-mode)
+           (untabify (point-min) (point-max)))
+   nil ))
 (provide 'init-ethan-wspace)
 ;;; init-ethan-wspace.el ends here
