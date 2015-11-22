3e42f3be66091f522e4d0def6c7085060f3fc037
Matthew Wampler-Doty <matthew.wampler.doty@gmail.com>
2015-11-22 11:51:46 -0700

    No more test refresh, different gpg key

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,14 +1,11 @@
 {:user {:plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
                   [lein-pprint "1.1.1"]
                   [lein-kibit "0.0.8"]
-                  [com.jakemccrary/lein-test-refresh "0.5.2"]
                   [com.palletops/lein-shorthand "0.4.0"]
-                  [refactor-nrepl "1.2.0-SNAPSHOT"]
-                  ]
-        :signing {:gpg-key "2ED94C0F"}
+                  [refactor-nrepl "2.0.0-SNAPSHOT"]]
+        :signing {:gpg-key "43FA2B12"}
         :dependencies [[alembic "0.3.2"]
                        [slamhound "1.5.5"]
-                       [org.clojure/tools.nrepl "0.2.7"]
                        [clj-stacktrace "0.2.8"]]
         :shorthand {. [alembic.still/distill alembic.still/lein]}}
  :injections [(let [orig (ns-resolve (doto 'clojure.stacktrace require)
