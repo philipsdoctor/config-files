e3554de2496c0a315ba5c35c81028d8626cc3e76
Matthew Wampler-Doty <matthew.wampler.doty@gmail.com>
2016-03-26 16:21:38 -0600

    Can't sort namespaces anymore

--- .emacs.d/init-modules/init-clojure.el
+++ .emacs.d/init-modules/init-clojure.el
@@ -6,9 +6,12 @@
 
 ;; Clojure mode
 (require 'bootstrap)
-(require-package 'clojure-mode 'cider 'flycheck 'evil 'multiple-cursors
-                 'clj-refactor 'inf-clojure 'smartparens)
 
+(require-package
+ 'clojure-mode 'cider 'flycheck 'evil
+ 'clj-refactor 'inf-clojure 'smartparens)
+
+(add-hook 'clojure-mode-hook 'smartparens-mode)
 
 ;; Use inferior clojure mode for clojurescript
 (add-hook 'clojurescript-mode-hook #'inf-clojure-minor-mode)
@@ -29,9 +32,6 @@
 (add-hook
  'clojure-mode-hook
  (lambda ()
-   ;;(make-local-variable 'evil-ex-commands)
-   ;;(setq evil-ex-commands (copy-list evil-ex-commands))
-
    ;; Next error
    (evil-ex-define-cmd "next-error" 'cider-jump-to-compilation-error)
 
@@ -47,30 +47,30 @@
    ;; Type :ns to change namespaces in cider repl
    (evil-ex-define-cmd "ns" 'cider-repl-set-ns)))
 
-;;;; Use light-table's command-return for evaluating in the REPL
+ ;;;; Use light-table's command-return for evaluating in the REPL
 (define-key clojure-mode-map
   command-eval-key
   (lambda () (interactive)
     (cond
      ;; When active region, evaluate region
-     ;;;; clojure-mode
+      ;;;; clojure-mode
      ((and mark-active
            (or (equal major-mode 'clojure-mode)
                (equal major-mode 'clojurec-mode)))
       (cider-eval-region (region-beginning) (region-end)))
-     ;;;; clojurescript-mode
+      ;;;; clojurescript-mode
      ((and mark-active (equal major-mode 'clojurescript-mode))
       ;;(inf-clojure-eval-region (region-beginning) (region-end))
       (execute-kbd-macro [?\C-c ?\C-r])
       )
 
      ;; When in normal evil mode and no expression selected, jigger cursor
-     ;;;; clojure-mode
+      ;;;; clojure-mode
      ((and (equal evil-state 'normal)
            (or (equal major-mode 'clojure-mode)
                (equal major-mode 'clojurec-mode)))
       (progn (forward-char) (cider-eval-last-sexp) (backward-char)))
-     ;;;; clojurescript-mode
+      ;;;; clojurescript-mode
      ((and (equal evil-state 'normal) (equal major-mode 'clojurescript-mode))
       (progn (forward-char)
              ;;(inf-clojure-eval-last-sexp)
@@ -78,12 +78,12 @@
              (backward-char)))
 
      ;; Default to evaluating last cursor
-     ;;;; clojure-mode
+      ;;;; clojure-mode
      ((or (equal major-mode 'clojure-mode)
           (equal major-mode 'clojurec-mode))
       (cider-eval-last-sexp))
 
-     ;;;; clojurescript-mode
+      ;;;; clojurescript-mode
      ((equal major-mode 'clojurescript-mode)
       (execute-kbd-macro [?\C-x ?\C-e])
       ;;(inf-clojure-eval-last-sexp)
@@ -95,24 +95,24 @@
   (lambda () (interactive)
     (cond
      ;; When active region, evaluate region
-     ;;;; clojure-mode
+      ;;;; clojure-mode
      ((and mark-active
            (or (equal major-mode 'clojure-mode)
                (equal major-mode 'clojurec-mode)))
       (cider-insert-in-repl (buffer-substring-no-properties
                              (region-beginning)
                              (region-end)) t))
-     ;;;; clojurescript-mode
+      ;;;; clojurescript-mode
      ((and mark-active (equal major-mode 'clojurescript-mode))
       (inf-clojure-eval-region (region-beginning) (region-end)))
 
      ;; When in normal evil mode and no expression selected, jigger cursor
-     ;;;; clojure-mode
+      ;;;; clojure-mode
      ((and (equal evil-state 'normal)
            (or (equal major-mode 'clojure-mode)
                (equal major-mode 'clojurec-mode)))
       (progn (forward-char) (cider-insert-last-sexp-in-repl t) (backward-char)))
-     ;;;; clojurescript-mode
+      ;;;; clojurescript-mode
      ((and (equal evil-state 'normal) (equal major-mode 'clojurescript-mode))
       (progn (forward-char)
              ;;(inf-clojure-eval-last-sexp) ; doesn't work use kbd-macro instead
@@ -120,11 +120,11 @@
              (backward-char)))
 
      ;; Default to evaluating last cursor
-     ;;;; clojure-mode
+      ;;;; clojure-mode
      ((or (equal major-mode 'clojure-mode)
           (equal major-mode 'clojurec-mode))
       (cider-insert-last-sexp-in-repl t))
-     ;;;; clojurescript-mode
+      ;;;; clojurescript-mode
      ((equal major-mode 'clojurescript-mode)
       ;;(inf-clojure-eval-last-sexp) ; doesn't work use kbd-macro instead
       (execute-kbd-macro [?\C-x ?\C-e])
@@ -135,19 +135,13 @@
   (sp-local-pair "`" nil :actions nil)
   (sp-local-pair "'" nil :actions nil))
 
-;;;; Use kibit for on the fly static analysis
+ ;;;; Use kibit for on the fly static analysis
 ;;(eval-after-load 'flycheck '(require-package 'kibit-mode))
 (add-hook 'clojure-mode-hook 'flycheck-mode)
 
 (defun clojure-indent-file ()
   "Indent a Clojure(Script) file."
-  (when (or (equal major-mode 'clojure-mode)
-            (equal major-mode 'clojurescript-mode))
-    (cljr-sort-ns))
-  (when (or (equal major-mode 'clojure-mode)
-            (equal major-mode 'clojurec-mode)
-            (equal major-mode 'clojurescript-mode))
-    (indent-region (point-min) (point-max))))
+  (indent-region (point-min) (point-max)))
 
 ;; Tidy up file on write
 (add-hook 'before-save-hook 'clojure-indent-file)
@@ -178,4 +172,4 @@
   (add-hook 'clojurescript-mode-hook 'set-cljs-dash-at-point-docset))
 
 (provide 'init-clojure)
-;;; init-clojure.el ends here
+ ;;; init-clojure.el ends here
